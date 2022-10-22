5ed82ad73a9abb7931d35ae006b595a8341431f7
UITQuang <19522094@gm.uit.edu.vn>
2022-10-22 08:30:33 +0700

    update login signup
    update login signup

--- lib/ui/login/login.dart
+++ lib/ui/login/login.dart
@@ -151,6 +151,7 @@ class _LoginPageState extends State<LoginPage> {
               GestureDetector(
                 onTap: (){
                   // get(SignUpPage);
+
                   Navigator.of(context).push(MaterialPageRoute(builder: (context)=> SignUpPage()));
 
                 },
--- lib/ui/login/signup.dart
+++ lib/ui/login/signup.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:flutter/material.dart';
-import 'package:project1/ui/login/login.dart';
+
+import 'login.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -35,75 +36,90 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                width: 300,
-                child: TextField(
-                  controller: nameControler,
-                  decoration: InputDecoration(
-                      labelText: 'Tên người dùng',
-                      prefixIcon: Icon(Icons.account_circle_outlined)),
-                ),
-              ),
-              Container(
-                width: 300,
-                child: TextField(
-                  controller: phoneControler,
-                  decoration: InputDecoration(
-                      labelText: 'Số điện thoại',
-                      prefixIcon: Icon(Icons.phone)),
-                ),
-              ),
-              Container(
-                width: 300,
-                child: TextField(
-                  controller: passwordControler,
-                  decoration: InputDecoration(
-                      labelText: 'Mật khẩu', prefixIcon: Icon(Icons.password)),
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              GestureDetector(
-                onTap: () {
-                  signup(
-                      nameControler.text.toString(),
-                      phoneControler.text.toString(),
-                      passwordControler.text.toString());
-                  // Navigator.of(context).push(MaterialPageRoute(builder: (context)=> LoginPage()));
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text('Đăng Ký'),
-                  width: 300,
-                  height: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-            ],
+  appBar:  AppBar(
+    backgroundColor: Colors.grey,
+    bottom: PreferredSize(
+      preferredSize: Size.fromHeight(5),
+      child: Container(
+        color: Colors.grey[200],
+        height: 1.0,
+      ),
+    ),
+    brightness: Brightness.light,
+    titleSpacing: 10,
+    automaticallyImplyLeading: true,
+    title: Text('Đăng Ký'),
+  ),
+      body: info()
+    );
+  }
+  Widget info(){
+    return SingleChildScrollView(
+        child:Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 50,
           ),
-        ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            width: 300,
+            child: TextFormField(
+              controller: nameControler,
+              decoration: InputDecoration(
+                  labelText: 'Tên người dùng',
+                  prefixIcon: Icon(Icons.account_circle_outlined)),
+            ),
+          ),
+          Container(
+            width: 300,
+            child: TextFormField(
+              controller: phoneControler,
+              decoration: InputDecoration(
+                  labelText: 'Số điện thoại',
+                  prefixIcon: Icon(Icons.phone)),
+            ),
+          ),
+          Container(
+            width: 300,
+            child: TextFormField(
+              obscureText: true,
+              controller: passwordControler,
+              decoration: InputDecoration(
+                  labelText: 'Mật khẩu', prefixIcon: Icon(Icons.password)),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          _signup(),
+        ],
+      ),
+    ));
+  }
+  Widget _signup(){
+    return  GestureDetector(
+      onTap: () {
+        signup(
+            nameControler.text.toString(),
+            phoneControler.text.toString(),
+            passwordControler.text.toString());
+
+        // Navigator.of(context).push(MaterialPageRoute(builder: (context)=> LoginPage()));
+      },
+      child: Container(
+        alignment: Alignment.center,
+        child: Text('Đăng Ký'),
+        width: 300,
+        height: 50,
+        decoration: BoxDecoration(
+            color: Colors.black12,
+            borderRadius: BorderRadius.circular(20)),
       ),
     );
   }
