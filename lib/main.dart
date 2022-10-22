a88987c4f1db3d0dae9e820e162ecf8ed81e8237
linh <nqlinh2205@gmail.com>
2022-10-22 19:21:39 +0700

    màn hình detail_peoduct

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'src/screen/ui/login/login.dart';
+import 'src/screen/detail_product.dart';
 
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(primaryColor: Colors.pinkAccent),
     debugShowCheckedModeBanner: false,
-    home: LoginPage(),
+    home: DetailProductScreen(),
   ));
 }
